1539ce1a93638c219072b8abbe6637e1e02d97fd
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2023-12-06 10:29:14 -0600

    Working exapmle

--- dagster_htmdec/assets.py
+++ dagster_htmdec/assets.py
@@ -1,6 +1,18 @@
-from dagster import asset
-from pandas import DataFrame, read_html, get_dummies
+import io
+from typing import Tuple
+
+from dagster import (
+    AssetOut,
+    Definitions,
+    EnvVar,
+    FilesystemIOManager,
+    asset,
+    multi_asset,
+)
+from pandas import DataFrame, get_dummies, read_csv, read_html
 from sklearn.linear_model import LinearRegression as Regression
+
+from .io_manager import ConfigurableGirderIOManager
 
 
 @asset
@@ -28,8 +40,37 @@ def change_model(country_stats: DataFrame) -> Regression:
     return Regression().fit(dummies, data["pop_change"])
 
 
-@asset
-def continent_stats(country_stats: DataFrame, change_model: Regression) -> DataFrame:
+@multi_asset(
+    outs={
+        "continent_stats_df": AssetOut(io_manager_key="fs_io_manager"),
+        "continent_stats_csv": AssetOut(io_manager_key="girder_io_manager"),
+    }
+)
+def continent_stats(
+    country_stats: DataFrame, change_model: Regression
+) -> Tuple[DataFrame, io.BytesIO]:
     result = country_stats.groupby("continent").sum()
     result["pop_change_factor"] = change_model.coef_
-    return result
+    fp = io.BytesIO()
+    result.to_csv(fp)
+    fp.seek(0)
+    return result, fp
+
+
+@asset(io_manager_key="girder_io_manager")
+def stats_read_from_girder(continent_stats_csv: io.BytesIO) -> None:
+    df = read_csv(continent_stats_csv)
+    print(df.head())
+
+
+defs = Definitions(
+    assets=[country_stats, change_model, continent_stats, stats_read_from_girder],
+    resources={
+        "fs_io_manager": FilesystemIOManager(),
+        "girder_io_manager": ConfigurableGirderIOManager(
+            api_key=EnvVar("GIRDER_API_KEY"),
+            api_url=EnvVar("GIRDER_API_URL"),
+            folder_id=EnvVar("GIRDER_FOLDER_ID"),
+        ),
+    },
+)
--- dagster_htmdec/io_manager.py
+++ dagster_htmdec/io_manager.py
@@ -1,19 +1,21 @@
 import io
 import os
+import urllib.parse as parse
 from dagster import (
     IOManager,
     ConfigurableIOManagerFactory,
+    MetadataValue,
     OutputContext,
     InputContext,
-    EnvVar,
 )
 from girder_client import GirderClient
 
 
 class GirderIOManager(IOManager):
-    def __init__(self, api_url, api_key):
+    def __init__(self, api_url, api_key, folder_id):
         self._cli = GirderClient(apiUrl=api_url)
         self._cli.authenticate(apiKey=api_key)
+        self.folder_id = folder_id
 
     def _get_path(self, context) -> str:
         if context.has_partition_key:
@@ -22,41 +24,68 @@ class GirderIOManager(IOManager):
             return "/".join(context.asset_key.path)
 
     def handle_output(self, context: OutputContext, obj):
+        if not obj:
+            return
         path = self._get_path(context)
+        name = ".".join(os.path.basename(path).rsplit("_", 1))
         size = obj.seek(0, os.SEEK_END)
         obj.seek(0)
-        self._cli.uploadStreamToFolder(
-            "656fb6af6292cc8a7fc5ccf6",
+
+        print(dir(context))
+        print(context.config)
+        print(context.metadata)
+        print(context.version)
+
+        fobj = self._cli.uploadStreamToFolder(
+            self.folder_id,
             obj,
-            os.path.basename(path),
+            name,
             size=size,
+            mimeType="text/csv",
         )
+        girder_metadata = {
+            "code_version": context.version,
+            "run_id": context.run_id,
+        }
+        self._cli.addMetadataToItem(fobj["itemId"], girder_metadata)
+        girder_url = parse.urlparse(self._cli.urlBase)
+        metadata = {
+            "size": fobj["size"],
+            "item_url": MetadataValue.url(
+                f"{girder_url.scheme}://{girder_url.netloc}/#item/{fobj['itemId']}"
+            ),
+            "download_url": MetadataValue.url(
+                f"{self._cli.urlBase}file/{fobj['_id']}/download"
+            ),
+        }
+        if context.has_asset_key:
+            context.add_output_metadata(metadata)
 
     def load_input(self, context: InputContext):
         path = self._get_path(context)
-        children = list(
-            self._cli.listItem("656fb6af6292cc8a7fc5ccf6", name=os.path.basename(path))
-        )
+        name = ".".join(os.path.basename(path).rsplit("_", 1))
+        children = list(self._cli.listItem(self.folder_id, name=name))
         if len(children) != 1:
             raise Exception(
                 f"Expected to find exactly one item at path {path}, but found {len(children)}"
             )
 
-        files = self.cli.get("item/{}/files".format(children[0]["_id"]))
+        files = self._cli.get("item/{}/files".format(children[0]["_id"]))
         if len(files) != 1:
             raise Exception(
                 f"Expected to find exactly one file at path {path}, but found {len(files)}"
             )
 
         fp = io.BytesIO()
-        self.cli.downloadFile(files[0]["_id"], fp)
+        self._cli.downloadFile(files[0]["_id"], fp)
         fp.seek(0)
         return fp
 
 
 class ConfigurableGirderIOManager(ConfigurableIOManagerFactory):
-    api_key: str = EnvVar("GIRDER_API_KEY")
-    api_url: str = EnvVar("GIRDER_API_URL")
+    api_key: str
+    api_url: str
+    folder_id: str
 
     def create_io_manager(self, context) -> GirderIOManager:
-        return GirderIOManager(self.api_url, self.api_key)
+        return GirderIOManager(self.api_url, self.api_key, self.folder_id)
